121e3bf1e6271e8b3bbb26e59afc9fae884f1d56
Shah Muhammad Rubayet Alam <rubayetalam21@gmail.com>
2025-06-13 20:46:59 +0600

    Slider has been added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import Slider from '../components/Slider';
+import AvailableFoods from './AvailableFoods';
+import { Fade, Slide } from 'react-awesome-reveal';
 
 const Home = () => {
     return (
-        <div>
-            
+        <div className='w-11/12 mx-auto'>
+            <div className='py-5'>
+                <Fade direction="up" duration={800} triggerOnce>
+                    <Slider />
+                </Fade>
+            </div>
+
+            <Slide direction="up" triggerOnce>
+                <AvailableFoods></AvailableFoods>
+            </Slide>
+           
         </div>
     );
 };
---
+++ src/components/Slider.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+const Slider = () => {
+    return (
+        <div className="w-full max-w-7xl mx-auto py-4">
+            <Swiper
+                modules={[Navigation, Pagination, Autoplay]}
+                spaceBetween={30}
+                slidesPerView={1}
+                navigation
+                pagination={{ clickable: true }}
+                autoplay={{ delay: 3000 }}
+                loop
+                className="rounded-xl overflow-hidden"
+            >
+                <SwiperSlide>
+                    <img
+                        src="https://i.ibb.co/svBCN4M1/Gaming.jpg"
+                        alt="Tech Gadget Box"
+                        className="w-full h-[400px] object-cover"
+                    />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img
+                        src="https://i.ibb.co/r2mZ9XS5/running.jpg"
+                        alt="Organic Beauty Box"
+                        className="w-full h-[400px] object-cover"
+                    />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img
+                        src="https://i.ibb.co/tpf32Dpv/reading.jpg"
+                        alt="Book Lovers Box"
+                        className="w-full h-[400px] object-cover"
+                    />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img
+                        src="https://i.ibb.co/yFVPyXR2/fishing.jpg"
+                        alt="Book Lovers Box"
+                        className="w-full h-[400px] object-cover"
+                    />
+                </SwiperSlide>
+               
+            </Swiper>
+        </div>
+    );
+};
+
+export default Slider;
