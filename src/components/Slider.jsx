f1cc43503e7c8666876236208e972b7a05584140
Shah Muhammad Rubayet Alam <rubayetalam21@gmail.com>
2025-06-17 00:43:36 +0600

    Slider images has been changed

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -20,28 +20,28 @@ const Slider = () => {
             >
                 <SwiperSlide>
                     <img
-                        src="https://i.ibb.co/svBCN4M1/Gaming.jpg"
+                        src="https://i.ibb.co/pr3rZjrs/1.jpg"
                         alt="Tech Gadget Box"
                         className="w-full h-[400px] object-cover"
                     />
                 </SwiperSlide>
                 <SwiperSlide>
                     <img
-                        src="https://i.ibb.co/r2mZ9XS5/running.jpg"
+                        src="https://i.ibb.co/v6xM5gn7/2.jpg"
                         alt="Organic Beauty Box"
                         className="w-full h-[400px] object-cover"
                     />
                 </SwiperSlide>
                 <SwiperSlide>
                     <img
-                        src="https://i.ibb.co/tpf32Dpv/reading.jpg"
+                        src="https://i.ibb.co/qYfT45NS/3.jpg"
                         alt="Book Lovers Box"
                         className="w-full h-[400px] object-cover"
                     />
                 </SwiperSlide>
                 <SwiperSlide>
                     <img
-                        src="https://i.ibb.co/yFVPyXR2/fishing.jpg"
+                        src="https://i.ibb.co/nSTBLvN/download.jpg"
                         alt="Book Lovers Box"
                         className="w-full h-[400px] object-cover"
                     />
